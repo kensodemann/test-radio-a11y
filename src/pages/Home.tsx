d1b5d747f6d19a802596abf89a395046709a0389
Ken Sodemann <ken@ionic.io>
2020-11-26 12:11:29 -0600

    use 5.5.1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonRadio,
+  IonRadioGroup,
+  IonTitle,
+  IonToolbar,
+  IonCheckbox
+} from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -12,12 +25,38 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          <IonRadioGroup name={'radio-group'}>
+            <IonListHeader>
+              <IonLabel>My Radio Group</IonLabel>
+            </IonListHeader>
+            <IonItem lines={'none'}>
+              <IonLabel>Option A</IonLabel>
+              <IonRadio value={'option-a'} />
+            </IonItem>
+            <IonItem lines={'none'}>
+              <IonLabel>Option B</IonLabel>
+              <IonRadio value={'option-b'} color={'danger'} />
+            </IonItem>
+            <IonItem lines={'none'}>
+              <IonLabel>Option C</IonLabel>
+              <IonRadio value={'option-c'} />
+            </IonItem>
+          </IonRadioGroup>
+        </IonList>
+
+        <IonList>
+          <IonItem>
+            <IonLabel>Item 1</IonLabel>
+            <IonCheckbox></IonCheckbox>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Item 2</IonLabel>
+            <IonCheckbox></IonCheckbox>
+          </IonItem>
+        </IonList>
+
+        <IonButton>PressMe!!</IonButton>
       </IonContent>
     </IonPage>
   );
